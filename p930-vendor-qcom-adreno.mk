5de5a863b688b90d4d882da11d185d95a8698de1
miroslav <miroslav@lenovo.(none)>
2014-04-09 03:00:52 +0300

    first commit

---
+++ p930-vendor-qcom-adreno.mk
@@ -0,0 +1,20 @@
+## EGL
+PRODUCT_COPY_FILES += \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libCB.so:system/lib/libCB.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libOpenCL.so:system/lib/libOpenCL.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libgsl.so:system/lib/libgsl.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    vendor/lge/p930/proprietary/qcom/adreno/system/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    vendor/lge/p930/proprietary/qcom/adreno/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    vendor/lge/p930/proprietary/qcom/adreno/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    vendor/lge/p930/proprietary/qcom/adreno/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+
